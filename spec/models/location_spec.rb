36236a067e9f81951667c119e270539373f32a43
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-29 19:49:42 -0300

    test: ensure be able to associate with survivor

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { 
+      should belong_to(:survivor)
+      .class_name('Survivor')
+      .with_foreign_key('survivor_id')  
+    }
+  end
 end
