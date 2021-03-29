52210bf68a302ea2b46a2fa3d30d53bb6c2f62ad
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-29 20:51:33 -0300

    feat: add flag relation with survivor

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -1,2 +1,5 @@
 class Flag < ApplicationRecord
+  belongs_to :flagger, class_name: 'Survivor', foreign_key: 'flagger_id'
+  belongs_to :flagged, class_name: 'Survivor', foreign_key: 'flagged_id'
+
 end
