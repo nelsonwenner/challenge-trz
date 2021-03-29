546ee6b1bec5f66ea8bbf52e34399c059e3a41c4
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-29 18:52:02 -0300

    feat: add model survivor

---
+++ db/migrate/20210329192457_create_survivors.rb
@@ -0,0 +1,12 @@
+class CreateSurvivors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :survivors do |t|
+      t.string :name
+      t.integer :age
+      t.integer :gender
+      t.references :location, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/survivors.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :survivor do
+    
+  end
+end
