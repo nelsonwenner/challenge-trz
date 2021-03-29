920adcadf02c1b1030ba7bf69641e0e5a3e1fca8
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-29 20:00:17 -0300

    test: ensure be able to validate location model attributes

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -8,4 +8,19 @@ RSpec.describe Location, type: :model do
       .with_foreign_key('survivor_id')  
     }
   end
+
+  describe 'Validations' do
+    it {should validate_presence_of(:latitude)}
+    it {should validate_presence_of(:longitude)}
+    it { 
+      should validate_numericality_of(:latitude)
+      .is_greater_than_or_equal_to(-90)
+      .is_less_than_or_equal_to(90)
+    }
+    it { 
+      should validate_numericality_of(:longitude)
+      .is_greater_than_or_equal_to(-180)
+      .is_less_than_or_equal_to(180)
+    }
+  end
 end
