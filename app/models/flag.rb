426a0c072221681b89fe6b579d7e260a8db03de2
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-30 09:35:42 -0300

    feat: add self flag survivor check

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -3,4 +3,14 @@ class Flag < ApplicationRecord
   belongs_to :flagged, class_name: 'Survivor', foreign_key: 'flagged_id'
 
   validates_uniqueness_of :flagger_id, :scope => [:flagged_id], message: 'Unique pair flag, A survivor cannot flagger the same flagged twice.'
+
+  validate :check_self_flag
+
+  private
+
+  def check_self_flag
+    if self.flagger_id == self.flagged_id
+      self.errors.add(:flag, 'You cannot self-flag')  
+    end
+  end
 end
