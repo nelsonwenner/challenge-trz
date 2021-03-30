3411a663e6dc38a03424cde45f2149325de2d75d
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-30 19:47:04 -0300

    feat: add relation variable with flag. flagger and flagged.

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -2,6 +2,9 @@ class Survivor < ApplicationRecord
   has_one :location, class_name: 'Location'
   has_many :resources, class_name: 'Resource'
 
+  has_many :flagger, class_name: 'Flag', foreign_key: 'flagger_id'
+  has_many :flagged, class_name: 'Flag', foreign_key: 'flagged_id'
+
   accepts_nested_attributes_for :location
   accepts_nested_attributes_for :resources
 
