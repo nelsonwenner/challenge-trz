c70ac93ff743524893df32f7ba39c7ea180c72da
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-30 00:32:24 -0300

    test: ensure be able to validate resource uniqueness pair

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -13,12 +13,23 @@ RSpec.describe Resource, type: :model do
       .with_foreign_key('item_id')  
     }
   end
+
   describe 'Validations' do    
     it { should validate_presence_of(:quantity) }
     it { 
       should validate_numericality_of(:quantity)
       .is_greater_than_or_equal_to(0)
       .only_integer() 
+    }
+  end
+  
+  describe 'Uniqueness' do
+    subject { create(:resource) }
+
+    it { 
+      should validate_uniqueness_of(:survivor_id)
+      .scoped_to(:item_id)
+      .with_message('Unique pair resource keys') 
     }
   end
 end
