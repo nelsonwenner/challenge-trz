d69e054906fca0c1d7e4f6ebfe5790b89d656bac
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-29 21:01:58 -0300

    feat: add flagger uniqueness pair

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -2,4 +2,5 @@ class Flag < ApplicationRecord
   belongs_to :flagger, class_name: 'Survivor', foreign_key: 'flagger_id'
   belongs_to :flagged, class_name: 'Survivor', foreign_key: 'flagged_id'
 
+  validates_uniqueness_of :flagger_id, :scope => [:flagged_id], message: 'Unique pair flag, A survivor cannot flagger the same flagged twice.'
 end
