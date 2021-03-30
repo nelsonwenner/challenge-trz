b94d44063c35e0fdec7eda2adc339be1d02a4db4
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-30 13:09:21 -0300

    refactor: remove some details has_one and has_many

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -1,6 +1,6 @@
 class Survivor < ApplicationRecord
-  has_one :location, class_name: 'Location', foreign_key: 'survivor_id'
-  has_many :resources, class_name: 'Resource', foreign_key: 'survivor_id'
+  has_one :location, class_name: 'Location'
+  has_many :resources, class_name: 'Resource'
 
   accepts_nested_attributes_for :location
   accepts_nested_attributes_for :resources
