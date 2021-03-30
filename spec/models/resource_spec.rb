ca4322f11ecc7bcd6103990453e5a3be460b9f9f
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-30 13:04:04 -0300

    refactor: remove somes verification belong_to, from the resource association test

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -4,13 +4,9 @@ RSpec.describe Resource, type: :model do
   describe 'Associations' do
     it { 
       should belong_to(:survivor)
-      .class_name('Survivor')
-      .with_foreign_key('survivor_id')  
     }
     it { 
       should belong_to(:item)
-      .class_name('Item')
-      .with_foreign_key('item_id')  
     }
   end
 
--- spec/models/survivor_spec.rb
+++ spec/models/survivor_spec.rb
@@ -5,12 +5,10 @@ RSpec.describe Survivor, type: :model do
     it { 
       should have_one(:location)
       .class_name('Location')
-      .with_foreign_key('survivor_id') 
     }
     it { 
       should have_many(:resources)
       .class_name('Resource')
-      .with_foreign_key('survivor_id') 
     }
   end
 
