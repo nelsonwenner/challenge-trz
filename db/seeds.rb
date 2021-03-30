90fc89ba4885a740580ebb71040d4fcdfbd25592
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-30 00:28:51 -0300

    chore: add item seeds and survivors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.create(name: 'Fiji Water', value: 14)
+Item.create(name: 'Campbell Soup', value: 12)
+Item.create(name: 'First Aid Pouch', value: 10)
+Item.create(name: 'AK47', value: 8)
+
+Survivor.create(name: 'Nelson', age: 28, gender: 'male')
+Survivor.create(name: 'Carlos', age: 30, gender: 'male')
