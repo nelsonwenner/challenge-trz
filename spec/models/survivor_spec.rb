f83515a4a2d19f0fde2881c9ee5aceaaec04b9e0
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-30 19:48:11 -0300

    test: ensure be able to validate relation flagger and flagged.

--- spec/models/survivor_spec.rb
+++ spec/models/survivor_spec.rb
@@ -7,8 +7,14 @@ RSpec.describe Survivor, type: :model do
       .class_name('Location')
     }
     it { 
-      should have_many(:resources)
-      .class_name('Resource')
+      should have_many(:flagger)
+      .class_name('Flag')
+      .with_foreign_key('flagger_id')
+    }
+    it { 
+      should have_many(:flagged)
+      .class_name('Flag')
+      .with_foreign_key('flagged_id')
     }
   end
 
