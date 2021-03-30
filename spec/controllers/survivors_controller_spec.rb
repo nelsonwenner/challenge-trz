d49021f04241980b2b528b385730607a89a019c0
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-30 13:51:13 -0300

    test: ensure not be able to create a new survivor

--- spec/controllers/survivors_controller_spec.rb
+++ spec/controllers/survivors_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::SurvivorsController, type: :controller do
   describe 'POST #create' do
     let(:valid_survivor) { attributes_for(:survivor) }
+    let(:invalid_survivor) { attributes_for(:survivor, name: -1, age: -1) }
 
     describe 'When attributes are valid' do
       it 'Should be able to create a new survivor' do
@@ -21,6 +22,14 @@ RSpec.describe Api::V1::SurvivorsController, type: :controller do
         expect{ 
           post :create, params: { survivor: valid_survivor }
         }.to change(Location, :count).by(1)
+      end
+    end
+
+    describe 'When attributes are invalid' do
+      it 'Should not be able to create a new survivor' do
+        expect{ 
+          post :create, params: { survivor: invalid_survivor }
+        }.not_to change(Survivor, :count)
       end
     end
   end
