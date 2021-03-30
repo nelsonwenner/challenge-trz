ecc5d9ccfbc82139558fdb56082999676d60b13d
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-29 22:24:54 -0300

    test: ensure be able to validate item model attributes

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do    
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:value) }
+    it { 
+      should validate_numericality_of(:value)
+      .is_greater_than_or_equal_to(0)
+      .only_integer() 
+    }
+  end
 end
