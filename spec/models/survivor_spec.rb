456ff35a92a080067be1f7c7950b4f850b56f2ba
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-29 22:43:09 -0300

    test: ensure be able to associeate with location and resources

--- spec/models/survivor_spec.rb
+++ spec/models/survivor_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Survivor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associantions' do 
+    it { 
+      should have_one(:location)
+      .class_name('Location')
+      .with_foreign_key('survivor_id') 
+    }
+    it { 
+      should have_many(:resources)
+      .class_name('Resource')
+      .with_foreign_key('survivor_id') 
+    }
+  end
 end
