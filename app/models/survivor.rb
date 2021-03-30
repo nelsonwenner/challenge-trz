845c330b814b5b865ec2c205142dc456384e9fe5
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-29 22:40:49 -0300

    feat: add survivor relations

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -1,2 +1,5 @@
 class Survivor < ApplicationRecord
+  has_one :location, class_name: 'Location', foreign_key: 'survivor_id'
+  has_many :resources, class_name: 'Resource', foreign_key: 'survivor_id'
+
 end
