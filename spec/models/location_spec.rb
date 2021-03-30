0c9c8a0e6b089b62bcdee9ce1b7de599944572a9
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-30 12:30:29 -0300

    refactor: remove somes verification belong_to, from the association test

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe Location, type: :model do
   describe 'Associations' do
     it { 
       should belong_to(:survivor)
-      .class_name('Survivor')
-      .with_foreign_key('survivor_id')  
     }
   end
 
