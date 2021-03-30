31e603a01936198d9ac9f19eabf416e11fc99866
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-29 22:57:27 -0300

    test: ensure be able to validate survivor model attributes

--- spec/models/survivor_spec.rb
+++ spec/models/survivor_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe Survivor, type: :model do
       .with_foreign_key('survivor_id') 
     }
   end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:name).on(:create) }
+    it { should validate_presence_of(:age).on(:create) }
+    it { 
+      should validate_numericality_of(:age)
+      .is_greater_than_or_equal_to(0)
+      .only_integer() 
+    }
+    it { should validate_presence_of(:gender).on(:create) }
+    it { 
+      should define_enum_for(:gender).with_values([:male, :female]) 
+    }
+  end
 end
