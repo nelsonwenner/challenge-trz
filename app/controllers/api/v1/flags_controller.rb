ad8f6d82d8adf9319f9553089914395cc10f0985
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-30 22:18:37 -0300

    feat: add create a flag in the flag controller

---
+++ app/controllers/api/v1/flags_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    class FlagsController < ApplicationController
+      def create
+        @flag = Flag.new(flag_params)
+
+        if @flag.save
+          render status: 201, json: @flag, serializer: FlagSerializer 
+        else
+          render status: 400, json: errors(@flag)
+        end
+      end      
+
+      private
+
+      def flag_params 
+        params.require(:flag).permit(:flagger_id, :flagged_id)
+      end
+
+      def errors(record)
+        { errors: record.errors.messages }
+      end
+    end
+  end
+end
