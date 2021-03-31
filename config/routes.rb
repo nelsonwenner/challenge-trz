dd21101542aff3989e2537cfeb512fc9c990d535
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-31 13:16:08 -0300

    chore: add route trade

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :survivors
       resources :flags, only: [:create]
+      resources :trades, only: [:create]
     end
   end
 end
