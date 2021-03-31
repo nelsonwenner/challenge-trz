fdd53df6a0bc41929d05b03302d15a5ab5015e7d
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-30 23:27:55 -0300

    test: ensure be able to infect a survivor with five flags

--- spec/controllers/flags_controller_spec.rb
+++ spec/controllers/flags_controller_spec.rb
@@ -2,23 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::FlagsController, type: :controller do
   describe 'POST #create' do
-    let(:survivor_flagger1) { create(:survivor) }
-    let(:survivor_flagger2) { create(:survivor) }
-    let(:survivor_flagger3) { create(:survivor) }
-    let(:survivor_flagger4) { create(:survivor) }
-    let(:survivor_flagger5) { create(:survivor) }
-    let(:survivor_target) { create(:survivor) }
+    let(:sender1) { create(:survivor) }
+    let(:sender2) { create(:survivor) }
+    let(:sender3) { create(:survivor) }
+    let(:sender4) { create(:survivor) }
+    let(:sender5) { create(:survivor) }
+    let(:target) { create(:survivor) }
 
     describe 'When attributes are valid' do
       it 'Should be able to create a new flag' do
         expect{ 
           post :create, params: { flag: 
             {
-              flagger_id: survivor_flagger1.id, 
-              flagged_id: survivor_flagger2.id
+              flagger_id: sender1.id, 
+              flagged_id: sender2.id
             } 
           }
         }.to change(Flag, :count).by(1)
+      end
+
+      it 'Should be able to infect a survivor with five flags'do
+        Flag.create(flagger_id: sender1.id, flagged_id: target.id)
+        Flag.create(flagger_id: sender2.id, flagged_id: target.id)
+        Flag.create(flagger_id: sender3.id, flagged_id: target.id)
+        Flag.create(flagger_id: sender4.id, flagged_id: target.id)
+
+        expect{
+          post :create, params: { flag: 
+            {
+              flagger_id: sender5.id, 
+              flagged_id: target.id
+            }
+          }
+          target.reload
+        }.to change(target, :infected).to(true)
       end
     end
   end
