6cf05a637a7a11dfc817136e08b5056df7282e96
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-30 21:19:52 -0300

    feat: add check flagger is infected

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -4,6 +4,7 @@ class Flag < ApplicationRecord
 
   validates_uniqueness_of :flagger_id, :scope => [:flagged_id], message: 'Unique pair flag, A survivor cannot flagger the same flagged twice.'
 
+  before_save :check_flagger_is_infected
   validate :check_self_flag
 
   private
@@ -11,6 +12,12 @@ class Flag < ApplicationRecord
   def check_self_flag
     if self.flagger_id == self.flagged_id
       self.errors.add(:flag, 'You cannot self-flag')  
+    end
+  end
+
+  def check_flagger_is_infected
+    if self.flagger.infected
+      self.errors.add(:flag, 'An infected survivor cannot signal')  
     end
   end
 end
