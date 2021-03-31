ba25e8bb4e6625a03732fc4437cb2740bedb5839
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-31 16:18:42 -0300

    test: ensure not be able to sender trade resources that you do not have the quantity

--- spec/controllers/trades_controller_spec.rb
+++ spec/controllers/trades_controller_spec.rb
@@ -12,13 +12,26 @@ RSpec.describe Api::V1::TradesController, type: :controller do
       it 'Should not be able to trade with a infected' do
         response = post :create, params: { trade: {
             sender_id: infected_id, 
-            sender_resources: [{id: 1, quantity: 10}],
+            sender_resources: [{item_id: 1, quantity: 10}],
             target_id: target.id, 
-            target_resources: [{id: 2, quantity: 10}]
+            target_resources: [{item_id: 2, quantity: 10}]
           }
         }
         expect(eval(response.body)).to eq(
           {'errors':'infected survivors cannot trade'}
+        )
+      end
+
+      it 'Should not be able to sender trade resources that you do not have the quantity' do
+        response = post :create, params: { trade: {
+            sender_id: sender.id, 
+            sender_resources: [{item_id: 1, quantity: 20}],
+            target_id: target.id, 
+            target_resources: [{item_id: 1, quantity: 10}]
+          }
+        }
+        expect(eval(response.body)).to eq(
+          {'errors':'sender does not have this resources quantity'}
         )
       end
     end
