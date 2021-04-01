788c6d0a636753e6b76cb63e813abe20755939be
wenner <nelsondiasdemedeiros@gmail.com>
2021-04-01 18:17:56 -0300

    feat: add relations with not infected resources

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  #Indicating the class where this model is referenced, you can apply
+  #joins to other references in this target table and apply conditions.
+  has_many :not_infected_resources, -> { 
+    joins(:survivor).where(survivors: { infected: false })
+  }, class_name: 'Resource'
+
   validates_presence_of :name, :value
   validates_numericality_of :value, greater_than_or_equal_to: 0, only_integer: true
 end
