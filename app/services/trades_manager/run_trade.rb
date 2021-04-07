7556106462421c64398775710dc86bd1231b12fb
wenner <nelsondiasdemedeiros@gmail.com>
2021-04-07 20:35:07 -0300

    refactor: removed some methods verification

--- app/services/trades_manager/run_trade.rb
+++ app/services/trades_manager/run_trade.rb
@@ -10,9 +10,10 @@ module TradesManager
     
     def call
       verify_survivors_infected(@sender, @target)
-      resources_checker(@sender, @sender_resources, 'sender')
-      resources_checker(@target, @target_resources, 'target')
-      points_checker(@sender_resources, @target_resources)
+
+      TradesManager::ResourcesChecker.call(@sender, @sender_resources, 'sender')
+      TradesManager::ResourcesChecker.call(@target, @target_resources, 'target')
+      TradesManager::PointsChecker.call(@sender_resources, @target_resources)
 
       ActiveRecord::Base.transaction do
         swap_resources(@sender, @sender_resources, @target)
@@ -32,45 +33,6 @@ module TradesManager
       OpenStruct.new({success?: true, payload: result})
     end
 
-    def verify_survivors_infected(sender, target)
-      if sender.infected or target.infected
-        raise Exception.new 'infected survivors cannot trade'
-      end
-    end
-
-    def resources_checker(survivor, resources, name)
-      resources.each { |sent_resource|
-        item_id = sent_resource[:item_id].to_i
-        quantity = sent_resource[:quantity].to_i
-
-        current_resource = survivor.resources.find { |i| i.item_id == item_id }
-
-        raise Exception.new("#{name} resource item_id #{item_id} not found"
-        ) if current_resource.nil?
-
-        if current_resource.quantity < quantity
-          raise Exception.new("#{name} does not have this resources quantity")
-        end
-      }
-    end
-    
-    def points_checker(sender_resources, target_resources)
-      sender_points = calculate_points(sender_resources)
-      target_points = calculate_points(target_resources)
-      
-      if sender_points != target_points
-        raise Exception.new(
-          "The scores of the target's resources are different from the sender"
-        )
-      end
-    end
-
-    def calculate_points(resources)
-      resources.reduce(0) { |memo, hash|
-        memo += Item.find(hash[:item_id].to_i).value * hash[:quantity].to_i
-      }
-    end
-
     def swap_resources(sender, sender_resources, target)
       sender_resources.each { |hash_resource|
         item_id = hash_resource[:item_id].to_i
@@ -87,6 +49,12 @@ module TradesManager
           target_resource.quantity += quantity
         end
       }
+    end
+
+    def verify_survivors_infected(sender, target)
+      if sender.infected or target.infected
+        raise Exception.new 'infected survivors cannot trade'
+      end
     end
 
     private
